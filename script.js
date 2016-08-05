3dde87ac9da4e48ecb7601db22f781d073a85354
Sean Wines <seanwinesdev@gmail.com>
2016-08-04 18:29:34 -0700

    Addedd user location

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var
   colors = ['#92bcff', '#d692ff', '	#ffd692', '	#ffa092'],
   userInput,
+  lat,
+  long,
   $welcome = $('.welcome'),
   $main = $('.main-content'),
   $searchText = $('.search-text'),
@@ -25,6 +27,44 @@ function round(item) {
   return Math.round(item);
 }
 
+function animateTop(item) {
+  $main.animate({
+    top: String(item)
+  });
+}
+
+function setData(json) {
+
+  if (json.message === 'Error: Not found city') {
+
+    $('.validator').fadeIn('slow').delay(700).fadeOut();
+
+  } else {
+
+    $('.hero i').hide().css('color', randomColor(colors)).fadeIn(1000);
+
+    if ($(window).width() > 480) {
+      animateTop(120);
+    } else {
+      animateTop(8);
+    }
+
+    $('.search-container').hide().fadeIn();
+    $cityName.text(json.name);
+    $('.weather-desc').text(json.weather[0].main);
+    $('.weather-icon').attr('src', 'http://openweathermap.org/img/w/' + json.weather[0].icon + '.png');
+    $('.weather-temp span').text(round(json.main.temp) + '°');
+    $('.max-temp').text(round(json.main.temp_max) + '°');
+    $('.min-temp').text(round(json.main.temp_min) + '°');
+    $('.weather-humidity span').text(json.main.humidity);
+
+    $('.deg span').text(json.wind.deg);
+    $('.gust span').text(json.wind.gust);
+    $('.speed span').text(json.wind.speed);
+    $('.weather-pressure span').text(json.main.pressure);
+  }
+}
+
 $(function() {
 
   preventEnter();
@@ -35,41 +75,32 @@ $(function() {
   //Show Main Content
   $main.delay(2000).fadeIn(800);
 
+  //Flash try 'me'
+  $('.try-type').fadeIn();
+
   $searchButton.on('click', function() {
     userInput = $searchText.val();
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&APPID=' + key, function(json) {
+    if (userInput === 'me') {
 
-      if (json.message === 'Error: Not found city') {
+      // Ger user's location
+      navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
 
-        $('.validator').fadeIn('slow').delay(700).fadeOut();
+        //User user's location to get API
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=' + key + '&units=imperial', function(json) {
+          setData(json);
+        })
+      });
 
-      } else {
+    } else {
 
-        $('.hero i').hide().css('color', randomColor(colors)).fadeIn(1000);
+      // User user input to get API
+      $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&APPID=' + key, function(json) {
+        setData(json);
+      })
+    }
 
-        if ($(window).width() > 480) {
-          $main.animate({
-            top: '120'
-          });
-        }
-
-        $('.search-container').hide().fadeIn();
-        $cityName.text(json.name);
-        $('.weather-desc').text(json.weather[0].main);
-        $('.weather-icon').attr('src', 'http://openweathermap.org/img/w/' + json.weather[0].icon + '.png');
-        $('.weather-temp span').text(round(json.main.temp) + '°');
-        $('.max-temp').text(round(json.main.temp_max) + '°');
-        $('.min-temp').text(round(json.main.temp_min) + '°');
-        $('.weather-humidity span').text(json.main.humidity);
-        $('.weather-pressure span').text(json.main.pressure);
-
-        $('.deg span').text(json.wind.deg)
-        $('.gust span').text(json.wind.gust)
-        $('.speed span').text(json.wind.speed)
-
-      }
-
-    });
   });
-})
+});
